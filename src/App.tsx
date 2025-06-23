97fb5140ac9fabb5e2edb061e9cad6c271ff8093
JOJOK63 <77489999+JOJOK63@users.noreply.github.com>
2025-06-23 19:05:05 +0200

    shuffle

--- src/App.tsx
+++ src/App.tsx
@@ -214,7 +214,12 @@ function App() {
   };
 
   const shuffleArray = (array: CardType[]) => {
-    return [...array].sort(() => Math.random() - 0.5);
+    const newArray = [...array];
+    for (let i = newArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [newArray[i], newArray[j]] = [newArray[j], newArray[i]]; // swap
+    }
+    return newArray;
   };
 
   const toggleShuffle = () => {
